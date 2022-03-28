27167c4c3642da5c1263ffe17c9365533820d8b2
Alexandra <sashika25@mail.ru>
2022-03-28 15:28:40 +0300

    new employee form and adding to db

--- src/main/java/com/my/springboot/petproject/rest/EmployeeController.java
+++ src/main/java/com/my/springboot/petproject/rest/EmployeeController.java
@@ -1,10 +1,13 @@
 package com.my.springboot.petproject.rest;
 
+import com.my.springboot.petproject.entity.Employee;
 import com.my.springboot.petproject.service.EmployeeServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 
@@ -18,6 +21,18 @@ public class EmployeeController {
     @GetMapping("/list")
     public String getEmployees(Model model) {
         model.addAttribute("employees", employeeService.findAll());
-        return "list-employees";
+        return "employees/list-employees";
+    }
+
+    @GetMapping("/showFormForAdd")
+    public String showFormForAdd(Model model) {
+        model.addAttribute("employee", new Employee());
+        return "employees/employee-form";
+    }
+
+    @PostMapping("/save")
+    public String saveEmployee(@ModelAttribute("employee") Employee employee) {
+        employeeService.save(employee);
+        return "redirect:/employees/list";
     }
 }
