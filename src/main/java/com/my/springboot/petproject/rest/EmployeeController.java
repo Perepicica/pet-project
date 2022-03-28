5c2e4a47c0db4946f170364fb3ae5b5f08d2a312
Alexandra <sashika25@mail.ru>
2022-03-28 16:13:10 +0300

    update employee data

--- src/main/java/com/my/springboot/petproject/rest/EmployeeController.java
+++ src/main/java/com/my/springboot/petproject/rest/EmployeeController.java
@@ -5,10 +5,7 @@ import com.my.springboot.petproject.service.EmployeeServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 
 @Controller
@@ -34,5 +31,12 @@ public class EmployeeController {
     public String saveEmployee(@ModelAttribute("employee") Employee employee) {
         employeeService.save(employee);
         return "redirect:/employees/list";
+    }
+
+    @GetMapping("/showFormForUpdate")
+    public String showFormForUpdate(@RequestParam("employeeId") int id,
+                                    Model model) {
+        model.addAttribute("employee", employeeService.findById(id));
+        return "employees/employee-form";
     }
 }
