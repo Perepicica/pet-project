a849d8075907924e3a62a5371517e4dcfa0f620d
Alexandra <sashika25@mail.ru>
2022-03-28 15:52:05 +0300

    sort list of employees by last name

--- src/main/java/com/my/springboot/petproject/dao/EmployeeRepository.java
+++ src/main/java/com/my/springboot/petproject/dao/EmployeeRepository.java
@@ -3,5 +3,9 @@ package com.my.springboot.petproject.dao;
 import com.my.springboot.petproject.entity.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
+
+    public List<Employee> findAllByOrderByLastNameAsc();
 }
